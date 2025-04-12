b51760d741a8220c1da9c1affe6ebeeeb8846f1c
Golu_singh <637golusingh@gmail.com>
2025-04-12 23:33:02 +0530

    everything get's fiexed only the css issue of 3 classes are updated at morning .

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -8,15 +8,15 @@ const Modal = ({ id, onclose, tag }) => {
       className={`fixed inset-0 z-50 bg-black/60 backdrop-blur-xs flex justify-center items-center ${tag ? '' : 'hidden'}`}
     >
       <div
-        className="bg-gradient-to-br from-green-500/5 to-cyan-500/5 border border-green-600 text-green-200 rounded-2xl p-1 w-[99%] h-[99%] m-1 min-h-72 shadow-[0_0_6px_#00ff88] flex flex-col justify-between items-center relative scrollbar-hide"
+        className="bg-gradient-to-br from-green-500/5 to-cyan-500/5 border border-green-600 text-green-200 rounded-2xl p-1 w-11/12 h-11/12 m-1 min-h-72 shadow-[0_0_6px_#00ff88] flex flex-col justify-between items-center relative scrollbar-hide"
         onClick={e => e.stopPropagation()}
       >
         <div className="mb-2 absolute -top-6 -right-4  bg-red-200 inline rounded-full p-2 z-20 active:scale-85">
           <X onClick={onclose} className="cursor-pointer text-red-400" />
         </div>
 
-        <div className="w-full flex justify-center items-center min-h-[200px]">
-          <div id={id} className="w-full min-h-96 h-[79vh] rounded-md" />
+        <div className="w-full flex justify-center items-center min-h-[200px] ">
+          <div id={id} className="w-full min-h-96 h-full rounded-md m-auto" />
         </div>
       </div>
     </div>,
