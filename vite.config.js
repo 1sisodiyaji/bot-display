e9bdabc6045e0f180b56530c58cf0a001559d2da
Golu_singh <637golusingh@gmail.com>
2025-04-12 16:00:54 +0530

    committed

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import strip from 'vite-plugin-strip';
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    react(),
-    // strip({
-    //   include: ['**/*.js', '**/*.ts', '**/*.jsx', '**/*.tsx'],
-    //   functions: ['console.log', 'console.debug', 'console.warn', 'console.info', 'debugger'],
-    // }),
+    react()
   ],
 });
