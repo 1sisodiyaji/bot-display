29fd5290c789fe823ccce1acce5171aafdc08d79
Golu_singh <637golusingh@gmail.com>
2025-04-13 14:33:21 +0530

    commiitted

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -7,7 +7,7 @@ const Modal = ({ id,tag }) => {
       className={`fixed inset-0 z-50 bg-black/60 backdrop-blur-xs flex justify-center items-center ${tag ? '' : 'hidden'}`}
     >
       <div
-        className="bg-gradient-to-br from-green-500/5 to-cyan-500/5 border border-green-600 text-green-200 rounded-2xl p-1 w-11/12 h-11/12 m-1 min-h-72 shadow-[0_0_6px_#00ff88] flex flex-col justify-between items-center relative scrollbar-hide"
+        className="bg-gradient-to-br from-green-500/5 to-cyan-500/5 border border-green-600 text-green-200  p-1 w-11/12 h-11/12 m-1 min-h-72 shadow-[0_0_6px_#00ff88] flex flex-col justify-between items-center relative scrollbar-hide"
         onClick={e => e.stopPropagation()}
       > 
         <div className="w-full flex justify-center items-center min-h-[90vh]">
